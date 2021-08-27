0cd3aae5d90e307b6ca7e952288e1386b75d8f11
Alex Schmid <aschmid13@gmail.com>
2021-08-27 11:21:16 -0700

    init

---
+++ visualizations/unique-count/index.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Card, CardBody, HeadingText, NrqlQuery, Spinner, AutoSizer, BillboardChart} from 'nr1';
+
+export default class UniqueCountVisualization extends React.Component {
+    // Custom props you wish to be configurable in the UI must also be defined in
+    // the nr1.json file for the visualization. See docs for more details.
+    static propTypes = {
+        /**
+         * An array of objects consisting of a nrql `query` and `accountId`.
+         * This should be a standard prop for any NRQL based visualizations.
+         */
+        nrqlQueries: PropTypes.arrayOf(
+            PropTypes.shape({
+                accountId: PropTypes.number,
+                query: PropTypes.string,
+            })
+        ),
+    };
+
+    render() {
+        const {nrqlQueries} = this.props;
+
+        const nrqlQueryPropsAvailable =
+            nrqlQueries &&
+            nrqlQueries[0] &&
+            nrqlQueries[0].accountId &&
+            nrqlQueries[0].query;
+
+        if (!nrqlQueryPropsAvailable) {
+            return <EmptyState />;
+        }
+
+        return (
+            <AutoSizer>
+                {({width, height}) => (
+                    <NrqlQuery
+                        query={nrqlQueries[0].query}
+                        accountId={parseInt(nrqlQueries[0].accountId)}
+                        pollInterval={NrqlQuery.AUTO_POLL_INTERVAL}
+                    >
+                        {({data, loading, error}) => {
+                            if (loading) {
+                                return <Spinner />;
+                            }
+
+                            if (error) {
+                                return <ErrorState />;
+                            }
+                            console.log(data[0])
+                            let billboarddata = [
+                                {
+                                    metadata: {
+                                    id: 'uniques',
+                                    name: 'uniques',
+                                    viz: 'main',
+                                    // units_data: {
+                                    //     y: 'MS',
+                                    // },
+                                    },
+                                    data: [
+                                    { y: data[0].data.length }, // Current value.
+                                    ],
+                                },
+                                ];
+
+                            return (
+                                <div>
+                                    <BillboardChart data={billboarddata}></BillboardChart>
+                                </div>
+                            );
+                        }}
+                    </NrqlQuery>
+                )}
+            </AutoSizer>
+        );
+    }
+}
+
+const EmptyState = () => (
+    <Card className="EmptyState">
+        <CardBody className="EmptyState-cardBody">
+            <HeadingText
+                spacingType={[HeadingText.SPACING_TYPE.LARGE]}
+                type={HeadingText.TYPE.HEADING_3}
+            >
+                Please provide at least one NRQL query & account ID pair
+            </HeadingText>
+            <HeadingText
+                spacingType={[HeadingText.SPACING_TYPE.MEDIUM]}
+                type={HeadingText.TYPE.HEADING_4}
+            >
+                An example NRQL query you can try is:
+            </HeadingText>
+            <code>
+            FROM Log select uniques(logCount) SINCE 1 day ago
+            </code>
+        </CardBody>
+    </Card>
+);
+
+const ErrorState = () => (
+    <Card className="ErrorState">
+        <CardBody className="ErrorState-cardBody">
+            <HeadingText
+                className="ErrorState-headingText"
+                spacingType={[HeadingText.SPACING_TYPE.LARGE]}
+                type={HeadingText.TYPE.HEADING_3}
+            >
+                Oops! Something went wrong.
+            </HeadingText>
+        </CardBody>
+    </Card>
+);
